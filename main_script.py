942508a64293909396ba9d33557d3c5e2f500485
Nikit Saraf <nikit.saraf@gmail.com>
2014-11-09 23:17:17 +0530

    Fix the issue in the main_script to save csv correctly

--- main_script.py
+++ main_script.py
@@ -2,12 +2,12 @@ import csv
 from topsy_scraping import *
 
 with open('Hinglish_words.csv', 'rb') as csvfile:
-	spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+	spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
 	for row in spamreader:
-		t = get_tweets_by_query(', '.join(row), 10)
+		t = get_tweets_by_query(row[0], 10)
 		for b in t:
-			print b['content']
+			print b['content'].encode('utf-8')
 			with open('dataset.csv', 'a') as csvfile:
-				spamwriter = csv.writer(csvfile, delimiter=' ', quotechar=, quoting=csv.QUOTE_MINIMAL)
-				spamwriter.writerow(b['content'].encode('utf-8'))
+				spamwriter = csv.writer(csvfile)
+				spamwriter.writerow([b['content'].encode('utf-8')])
         # print ', '.join(row)
