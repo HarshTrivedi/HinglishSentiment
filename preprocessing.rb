371c375b5ba0269aa056d55f969f4e7fbbc38dff
HarshTrivedi <harshjtrivedi94@gmail.com>
2014-11-30 15:02:16 +0530

    update script so as to ignore the nil labels

--- preprocessing.rb
+++ preprocessing.rb
@@ -1,23 +1,15 @@
 
 require 'csv'
-
-
 updated_csv_arrays = []
 
 CSV.foreach('initial_data.csv') do |row|
-	tweet = row[0]
-	label = row[1]
-	updated_tweet = tweet.gsub(/(@\w+)|(http:\/\/t.co\/\w*)/ , "" )
-	updated_csv_arrays << [ updated_tweet , row[1].to_i ]
-	puts "---start---"
-	puts tweet
-	puts "---"
-	puts updated_tweet
-	puts "---end---"
-	puts ""
+	if not( row[1].nil? or row[1].strip.empty? )
+		tweet = row[0]
+		label = row[1]
+		updated_tweet = tweet.gsub(/(@\w+)|(http:\/\/t.co\/\w*)/ , "" )
+		updated_csv_arrays << [ updated_tweet , row[1].to_i ]
+	end
 end
-
-
 
 CSV.open('preprocessed_data.csv', 'w') do |csv_object|
   updated_csv_arrays.each do |row_array|
