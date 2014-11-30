77119ad68f13203dbea923696bbce5b083de0350
HarshTrivedi <harshjtrivedi94@gmail.com>
2014-11-30 18:40:41 +0530

    add code for implementing emoticon features

--- classifier.py
+++ classifier.py
@@ -6,6 +6,12 @@ from nltk.corpus import movie_reviews
 def word_feats(words):
     return dict([(word, True) for word in words])
 
+def emoti_feats( tweet , pos , neg , unknown):
+	return { "pos_emot": int(pos) , "neg_emot": int(neg) , "unknown_emot": int(unknown) } 
+
+def happy_expression_feats( tweet , expression_count):
+	return { "happy_expression": int(expression_count) } 
+
 neg_feat = []
 pos_feat = []
 neutral_feat = []
@@ -13,12 +19,13 @@ with open('initial_data.csv', 'rb') as csvfile:
 	spamreader = csv.reader(csvfile, delimiter=',')
 	for row in spamreader:
 		if row[1] != "":
+			all_feats = dict(  word_feats(row[0].split()).items() #+ emoti_feats(row[0] , row[2] , row[3] , row[4]).items  )
 			if int(row[1]) == 0:
-				neutral_feat.append((word_feats(row[0].split()), 'neutral'))
+				neutral_feat.append( all_feats , 'neutral'))
 			if int(row[1]) == 1:
-				pos_feat.append((word_feats(row[0].split()), 'pos'))
+				pos_feat.append( all_feats , 'pos'))
 			if int(row[1]) == 2:
-				neg_feat.append((word_feats(row[0].split()), 'neg'))
+				neg_feat.append( all_feats , 'neg'))
 
 print len(neg_feat)
 print len(pos_feat)
@@ -30,8 +37,9 @@ neutralcutoff = len(neutral_feat)*8/10
 
 trainfeats = neg_feat[:negcutoff] + pos_feat[:poscutoff] + neutral_feat[:neutralcutoff]
 testfeats = neg_feat[negcutoff:] + pos_feat[poscutoff:] + neutral_feat[neutralcutoff:]
+
 print 'train on %d instances, test on %d instances' % (len(trainfeats), len(testfeats))
- 
+print trainfeats 
 classifier = NaiveBayesClassifier.train(trainfeats)
 print 'accuracy:', nltk.classify.util.accuracy(classifier, testfeats)
 classifier.show_most_informative_features()
